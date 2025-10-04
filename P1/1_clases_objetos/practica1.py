d8ba3bba85ec4187070a10c8fcf892f399f39fe0
LuisJimenez66 <luis_3141240113@utd.edu.mx>
2025-10-04 17:36:44 -0600

    Add files via upload

---
+++ P1/1_clases_objetos/practica1.py
@@ -0,0 +1,26 @@
+
+base=float(input("Valor Base: "))
+altura=float(input("Valor Altura: "))
+
+def Rectangulo(base,altura):
+    area=base*altura
+    return areaa
+
+areaa=Rectangulo(base,altura)
+print(f"Area: {areaa}")
+
+
+
+class Rectangulos:
+    def __init__(self,base,altura):
+        self.base=base
+        self.altura=altura
+    def area(self):
+        return self.base*self.altura
+    
+
+
+rect1=Rectangulos(base,altura)
+print(f"Area: {rect1.area()}")
+
+
